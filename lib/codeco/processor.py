7536f1cbb25e5d5271b1d680077e3de5cf6260c3
Carlos Jenkins <carlos.jenkins@hp.com>
2014-03-19 15:46:03 -0600

    First draft of the processor class.

---
+++ lib/codeco/processor.py
@@ -0,0 +1,111 @@
+# Copyright (C) 2014 Carlos Jenkins <carlos@jenkins.co.cr>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+Main processing module.
+"""
+
+import re
+
+from markdown import markdown
+from pygments import lexers, highlight
+
+
+class Processor(object):
+
+    ann_regex = \
+        r'^<\[annotation\]> *?' \
+        r'((?P<option>(line|chars))? *?(?P<args>[0-9,]+)?)? *?$'
+    ann_re = re.compile(ann_regex)
+
+    def render_markdown(self, body):
+        return markdown(body, output_format='html4')  # Same as Pygments
+
+    def render_rest(self, body):
+        # FIXME Implement
+        raise NotImplementedError('renderer')
+
+    def process_files(self, codefn, annfn, **kwargs):
+        with open(codefn, 'r') as cf:
+            code = cf.read()
+        with open(annfn, 'r') as af:
+            annotations = af.read()
+        return self.process(code, annotations, codefn=codefn, **kwargs)
+
+    def process(
+            self, code, annotations,
+            codefn=None, ann_format='markdown', prefix=''):
+
+        # Guess programming language
+        # Warning: might raise pygments.util.ClassNotFound
+        if codefn is None:
+            lexer = lexers.guess_lexer(code)
+        else:
+            lexer = lexers.guess_lexer_for_filename(codefn, code)
+
+        # Parse annotations
+        current = None
+        buff = []
+        parsed_anns = []
+
+        for line in annotations.splitlines():
+            m = Processor.ann_re.match(line)
+            if not m:
+                buff.append(line)
+                continue
+
+            if current is not None:
+                parsed_anns.append((current, buff))
+            current = m.groupdict()
+            buff = []
+
+        if buff:
+            parsed_anns.append((current, buff))
+
+        # Render annotations
+        renderers = {
+            'markdown' : self.render_markdown,
+            'rest'     : self.render_rest,
+        }
+        renderer = renderers[ann_format]
+
+        rendered_anns = ['<div class="annotations">']
+        for meta, lines in parsed_anns:
+            body = '\n'.join(lines)
+            render = '\n'.join([
+                '<div class="annotation">',
+                '<span><pre>{}</pre></span>'.format(str(meta)),  # FIXME
+                '<div class="annotation_body">',
+                renderer(body),
+                '</div>',
+                '</div>',
+            ])
+            rendered_anns.append(render)
+        renderered_anns.append('</div>')
+
+        # Highlight code
+        options = {
+            'linenos'  : 'table',
+            'linespans': prefix + 'line'
+        }
+        formatter = HtmlFormatter(**options)
+        highlighted = highlight(code, lexer, formatter)
+
+        return {
+            'code'        : highlighted,
+            'annotations' : '\n'.join(rendered_anns),
+            'style'       : formatter.get_style_defs('table.highlighttable'),
+            'script'      : ''
+        }
