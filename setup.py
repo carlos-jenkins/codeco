28a38da2fa62dfcb3b74f58680cc09aad4f9d1be
Carlos Jenkins <carlos.jenkins@hp.com>
2014-03-25 16:58:55 -0600

    Bumping version number because I suck at PyPI.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='codeco',
-    version='1.0',
+    version='1.1',
     description=(
         'Universal code annotator using Pygments '
         'and reStructuredText or Markdown.'
