3471e159c87ad1f87cc3f9c58dd90d529d4d1fa5
Carlos Jenkins <carlos.jenkins@hp.com>
2014-03-25 19:31:06 -0600

    Bumping version number for PyPI.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='codeco',
-    version='1.1',
+    version='1.2',
     description=(
         'Universal code annotator using Pygments '
         'and reStructuredText or Markdown.'
